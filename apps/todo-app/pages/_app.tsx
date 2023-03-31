af964925f54eac0910855ce602da3ee9fe16f7c6
chimiWangchukWangdi <chimiwangchukw@gmail.com>
2023-03-31 13:42:04 +0600

    intial commit

--- apps/todo-app/pages/_app.tsx
+++ apps/todo-app/pages/_app.tsx
@@ -1,17 +1,13 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import './styles.css';
+import { Provider } from 'react-redux';
+import { store } from '../state/store';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Welcome to todo-app!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
 
